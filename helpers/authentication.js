f97b27d265e6f28ebeff47ed193ce20dd032f3f3
Di Huang <dihuang.cs@gmail.com>
2014-03-06 11:45:13 -0800

    Sorry node

--- helpers/authentication.js
+++
@@ -1,8 +0,0 @@
-module.exports.findByEmail = function findByEmail(email, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * GET home page.
- */
-
-module.exports.index = function(req, res) {
-  res.render('index', {
-    title: 'Copy It & Own It'
-  });
-};
